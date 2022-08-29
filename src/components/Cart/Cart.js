4b0f6661c87608df01975f393d8c19643d4c3615
Sara Fletcher Briñez <saflebri@gmail.com>
2022-08-29 17:55:59 -0500

    issue purchase order

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import { getFirestore, addDoc, collection } from 'firebase/firestore'
 import React, { useContext } from "react";
 import { CartContext } from "../../Context/CartContext";
 import ItemCart from '../ItemCart/ItemCart'
@@ -8,6 +9,27 @@ export default function Cart() {
 
     const { cartData, totalPrice } = useContext(CartContext);
 
+    const order = {
+
+        buyer: {
+            name: 'Sara',
+            phone: '78945',
+            email: 'sara@gmail.com',
+        },
+        items: cartData.map(item => ({ id: item.id, tittle: item.title, price: item.price, quantity: item.quantityToAdd })),
+        date: new Date(),
+        total: totalPrice(),
+    }
+
+    const handleClick = () => {
+
+        const db = getFirestore();
+        const ordersCollection = collection(db, 'orders');
+        addDoc(ordersCollection, order)
+            .then(({ id }) => console.log(id, order))
+    }
+
+    
 
     return (
 
@@ -21,6 +43,8 @@ export default function Cart() {
             }
             <div className="styleTotalPrice">
                 Total compra: ${totalPrice()}
+                <button className="styleTotalButton" onClick={handleClick}>EMITIR COMPRA</button>
+
             </div>
         </div>
 
